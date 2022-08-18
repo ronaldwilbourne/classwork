c78d94111d246f9a08477a4f037ccf3c4856446f
ronnie 96 <ronaldwilbourne@gmail.com>
2022-08-18 14:34:17 -0400

    jsreview

---
+++ app.js
@@ -0,0 +1,101 @@
+//let firstName;
+ 
+//bad!
+//var age;
+
+//good!
+//let age;
+//constant having same value
+//const age = 8;
+//const name="ron"
+//const name2='ron'
+//const name3='ron'
+ //string
+
+ let firstName= "Bob";
+
+ let lastName= "the builder";
+  let fullname=firstName + lastName
+  console.log(fullname)
+  //concatenation
+  let banana= "concatena"+"nation"
+   console.log(banana)
+   //pascal case
+   //myFavoriteDesert
+   //camelcase=myFavoriteDesert
+   //kebabcase=my-favorite-desert
+   //logicaloperators
+   //and
+   //or
+   //not
+    let fire = 0;//0
+    fire-=10//-10
+    //only for increamenting or decrementing by 1
+    fire--//increment by 1
+    fire++//decrementing by 1
+     fire=fire-10
+     console.log(fire)
+//when using conditions you can use loose comparisom==/or strict comparison=== asignmet operator=
+//loose comparison the are not the same type it converts(use coercion) but strict
+//!=
+//!==
+let num1 =6
+let num2 =3
+//console.log(num1< 10 &&num2 >1 )
+//console.log(num1==5 ||num2==5)
+//console.log(!(num1==num2))
+
+var num3=24
+var num4=32
+//exercise1
+var num5= num3 + num4
+//console.log(num5)
+//exercise2
+//console.log(num3>num4)
+//console.log(num3/num4)
+//console.log(num3< 10 &&num4 >1)
+//exercise3
+var numY=23
+var numZ=33
+var num11=45
+var num12=12
+var numA=11
+var numJ=12
+var num66=66
+var num55=55
+var num67=67
+var numL=56
+//exercise4
+//console.log((numY+numZ+num11+num12+num66)/5)
+//console.log((numA+numJ+num55+num67+numL)/5)
+avarage1 =35.8
+avarage2 =40.2
+var avarage =((avarage1 + avarage2)/2)
+//console.log(avarage)
+
+let number =2;
+switch (number) {
+    case 1:
+       // console.log('inside of case 1')
+    case 2:
+    //console.log('inside of case 2')
+    case 3:
+       // console.log('inside of case3')
+        if (number ===1){
+           // console.log('the number is 1')
+        }
+}
+//ternary operator
+
+let lighting='dark'
+ lighting=== 'dark' ? 'night time': 'day time'
+// console.log(dayOrNight)
+ /*let lighting='light
+ let dayOrNight;
+ if(lighting==='dark'){
+    dayOrNight='night time'
+}else if */
+
+let letter ='B'
+letter==='B'? 'consonant':'vowel'
+console.log('consonant')
---
+++ functions/app.js
@@ -0,0 +1,27 @@
+//hero.moveRight()
+//A reusable block of code designed to perform a single purpose
+//buildings blocks of a programme
+let moveRight;
+let moveLeft;
+//i.e console.log()
+//function declaration 
+//ie it doesnt matter where you use it
+function sayHello(){
+    console.log('hello')
+    return'hello'
+}
+//scope to function that has been declared on a varible cant be used outside that funtiob
+//apo ndani ya brackets
+//function expression 
+//be careful where you use it
+const sayHi = function(){
+    console.log('hi')
+    return'hi'
+}
+//arrow funtion
+const sayHey = () => 'hey' 
+console.log(sayHello())
+console.log(sayHi())
+console.log(sayHey())
+
+function add(num1, num2)
