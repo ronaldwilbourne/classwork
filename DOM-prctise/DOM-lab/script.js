99f1d0de591db8376403fe0ebcaca82c91f484b7
ronnie96 <ronaldwilbourne@gmail.com>
2022-08-30 15:00:43 -0400

    gridreview

--- DOM-prctise/DOM-lab/script.js
+++ DOM-prctise/DOM-lab/script.js
@@ -1,59 +1,3 @@
-
- // <!-- select and cache the main element varible name main el-->//
-  var mainEl = document.querySelector('main');{
-  console.log(mainEl)
-  }
-
-
-  //set the background color of the mainEl;to the value stored//
-
-  mainEl.style.backgroundColor = "var(--main-bg)"
-
-  // set the content of mainEl to <h1>sei rocks</h1>//
-  mainEl.innerHTML='<h1>SEI ROCKS</h1>'
-
-  //add a class of flex-ctr to mainEl//
-  mainEl.classList.add('flex-ctr');
-
-//select and cache the < id="top-menu">elemnt in a varible named topMenuEl<//
-let topMenuEl = document.querySelector('#top-menu')
-    console.log(topMenuEl)
-
-//set the height topMenuEl element to be 100%//
-topMenuEl.style.height = '100%';
-//set the backgroundColor and assign it a css variable//
-topMenuEl.style.backgroundColor = "var(--top-menu-bg)"
-//add a class flex-around to topMenuEl
-topMenuEl.classList.add('flex-around')
-
-
-//iterate over the entire menuLinks arry and for each"link" object
-//create an <> element
-  
-//var menuLinks = [
-  //{text: 'about', href: '/about'},
-  //{text: 'catalog', href: '/catalog'},
-  //{text: 'orders', href: '/orders'},
-  //{text: 'account', href: '/account'},
-//];
-document.getElementById('main')
-
-menuLinks.forEach(oders => {
-  
- const anchor = document.createElement('a')
-//console.log(anchor)
-//on the new elemnt add href attribute with its value set to href properties of the link object//
-anchor.href=oders.href
-  //set the new elemnet content to have value tothe text proprety link object//
-  anchor.innerText=oders.text
-  //console.log(anchor)
-
-//append the new elemnt to the topMenuEl
-topMenuEl.appendChild(anchor)
-console.log(anchor)
-  
-  
-})
 var menuLinks = [
   {text: 'about', href: '/about'},
   {text: 'catalog', href: '#', subLinks: [
@@ -71,7 +15,80 @@ var menuLinks = [
     {text: 'sign out', href: '/account/signout'},
   ]},
 ];
+ // <!-- select and cache the main element varible name main el-->//
+  var mainEl = document.querySelector('main');{
+  console.log(mainEl)
+  }
 
-const subMenuEl =document.querySelector(#sub-menu)
-  const menu = document.querySelector
+
+  //set the background color of the mainEl;to the value stored//
+
+  mainEl.style.backgroundColor = "var(--main-bg)"
+
+  // set the content of mainEl to <h1>sei rocks</h1>//
+  mainEl.innerHTML='<h1>SEI ROCKS</h1>'
+
+  //add a class of flex-ctr to mainEl//
+  mainEl.classList.add('flex-ctr');
+
+//select and cache the < id="top-menu">elemnt in a varible named topMenuEl<//
+let topMenuEl = document.getElementById('top-menu')
+    console.log(topMenuEl)
+
+//set the height topMenuEl element to be 100%//
+topMenuEl.style.height = '100%';
+//set the backgroundColor and assign it a css variable//
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)"
+//add a class flex-around to topMenuEl
+topMenuEl.classList.add('flex-around')
+
+
+//iterate over the entire menuLinks arry and for each"link" object
+//create an <> element
+  
+
+menuLinks.forEach((orders) => {
+  
+ const anchor = document.createElement('a')
+//console.log(anchor)
+//on the new elemnt add href attribute with its value set to href properties of the link object//
+anchor.setAttribute ('href', orders.href)
+  //set the new elemnet content to have value tothe text proprety link object//
+  anchor.innerText=orders.text
+  //console.log(anchor)
+
+//append the new elemnt to the topMenuEl
+topMenuEl.appendChild(anchor) 
+})
+  
+  
+
+
+//select and cache < id ="sub-menu" >elemnt in a variable nameed subMneuEl
+const subMenuEl =document.getElementById("sub-menu")
+console.log(subMenuEl)  
+// set the height of subMenuEl element to 100
+subMenuEl.style.height = '100%'
+//set the background color of subMenuEl element to value stored in t he --sub-menu--bg CSS property
+subMenuEl.style.backgroundColor = "var(--top-menu-bg)"
+//add th4e class of flex- around to the subMenuEl element
+subMenuEl.classList.add('flex-around')
+//4.4 set css position of the subMenuElto the valueof absolute
+subMenuEl.style.cssPosition = "absolute"
+//4.5 css  property of the subMenuEl to value of 0
+subMenuEl.style.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
